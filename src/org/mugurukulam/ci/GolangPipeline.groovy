e37b2773e3deb866c32533d45ef071df39b87bf0
Suraj Tripathi <suraj.tripathi.snaatak@mygurukulam.co>
2026-04-30 02:08:47 +0530

    create vars and src

---
+++ src/org/mugurukulam/ci/GolangPipeline.groovy
@@ -0,0 +1,138 @@
+package org.company.ci
+
+class GolangPipeline implements Serializable {
+
+    def script
+
+    GolangPipeline(script){
+        this.script = script
+    }
+
+
+    def initialize(){
+
+        script.stage('Initialize') {
+
+            script.cleanWs()
+
+        }
+
+    }
+
+
+
+    def checkoutCode(config){
+
+        script.stage('Checkout Code') {
+
+            script.git(
+                url: config.GIT_REPO_URL,
+                branch: config.GIT_BRANCH
+            )
+
+        }
+
+    }
+
+
+
+    def setupGo(config){
+
+        script.stage('Setup Go Environment') {
+
+            def goHome = script.tool config.GO_TOOL_NAME
+
+            script.env.PATH =
+                    "${goHome}/bin:${script.env.PATH}"
+
+        }
+
+    }
+
+
+
+    def compile(){
+
+        script.stage('Code Compilation') {
+
+            script.sh '''
+            set -e
+            go mod tidy
+            go build ./...
+            '''
+
+        }
+
+    }
+
+
+
+    def notifySlack(config,status){
+
+        script.stage('Slack Notification') {
+
+            script.slackSend(
+                channel: config.SLACK_CHANNEL,
+
+                message:
+                "*${status}* - ${script.env.JOB_NAME} #${script.env.BUILD_NUMBER}\n${script.env.BUILD_URL}",
+
+                color:
+                status == 'SUCCESS' ?
+                'good' :
+                'danger'
+            )
+
+        }
+
+    }
+
+
+
+    def cleanup(){
+
+        script.cleanWs()
+
+    }
+
+
+
+    def execute(config){
+
+        try {
+
+            initialize()
+
+            checkoutCode(config)
+
+            setupGo(config)
+
+            compile()
+
+            script.currentBuild.result='SUCCESS'
+        }
+
+        catch(Exception e){
+
+            script.currentBuild.result='FAILURE'
+
+            script.echo "Build failed: ${e}"
+
+            script.error("Pipeline Failed")
+
+        }
+
+        finally {
+
+            def status =
+                script.currentBuild.result ?: 'SUCCESS'
+
+            notifySlack(config,status)
+
+            cleanup()
+
+        }
+
+    }
+
+}
---
+++ vars/golangCI.groovy
@@ -0,0 +1,34 @@
+import org.company.ci.GolangPipeline
+
+def call(Map config=[:]) {
+
+    node {
+
+        def defaultConfig = [
+
+            GIT_REPO_URL:
+            'https://github.com/suraj8957/employee-api.git',
+
+            GIT_BRANCH:
+            'main',
+
+            GO_TOOL_NAME:
+            'go',
+
+            SLACK_CHANNEL:
+            '#ci-operation-notifications'
+
+        ]
+
+
+        config = defaultConfig + config
+
+
+        def pipeline =
+            new GolangPipeline(this)
+
+        pipeline.execute(config)
+
+    }
+
+}
